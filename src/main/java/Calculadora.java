a84b52d0dedd6c0b4d7f5f86654c7ba377ecd878
alesolisb <alesolisb01@gmail.com>
2023-10-05 20:46:19 -0600

    Reconocimiento de texto en una imagen con Tesseract OCR

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,7 +1,11 @@
+import java.io.File;
+
 public class Calculadora {
     public static void main(String[] args) {
 
         System.out.println("Calculadora");
-
+        File image = new File("C:\\Users\\aleso\\OneDrive\\Escritorio\\op.png");
+        ReconText reconText = new  ReconText(image);
+        System.out.println(reconText.getText());
     }
 }
